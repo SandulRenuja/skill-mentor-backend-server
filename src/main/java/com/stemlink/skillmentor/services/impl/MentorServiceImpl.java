6b43455543b9dfaeefbec130a3fb14efde2fc640
Achintha Bandara <achinthabandara1@gmail.com>
2026-01-31 07:51:11 +0530

    added missing impoert

--- src/main/java/com/stemlink/skillmentor/services/impl/MentorServiceImpl.java
+++ src/main/java/com/stemlink/skillmentor/services/impl/MentorServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
